2b1ec4b01c222a236cdd874cbdb075555ed1fc12
lochana <lochanamethsilu@gmail.com>
2025-02-06 14:48:19 +0530

    Volunteer Sign Up page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero";
+import VolunteerSignUpPage from "./pages/VolunteerSignUpPage";
+import OrgSignUp from "./components/forms/OrgSignUp";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Hero />} />
+
+        <Route path="/signup" element={<VolunteerSignUpPage/>}/>
+        <Route path="/orgSignup" element={<OrgSignUp/>} />
       </Routes>
     </Router>
   );
