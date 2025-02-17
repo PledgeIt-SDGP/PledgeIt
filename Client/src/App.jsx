28c9e2ac64448ac30915c8adb1b4f3303ecca46e
dulansa1 <navindeedulansa@gmail.com>
2025-02-17 10:15:14 +0530

    Updated the App.jsx by rmoving Search.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Hero from "./components/hero/HeroContent";
 import { FooterWithSitemap } from "./components/nav&footer/Footer";
-import Search from "./components/search/Search";
+import SearchFilters from "./components/search/SearchFilters";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Hero />} />
-        <Route path="/event" element={<Search />} />
+        <Route path="/event" element={<SearchFilters />} />
       </Routes>
       <FooterWithSitemap />
     </Router>
