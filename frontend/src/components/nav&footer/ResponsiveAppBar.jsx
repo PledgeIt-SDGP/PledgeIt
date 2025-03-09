e26313e6c579e4c79192bed0576a72827dbfc55a
Dulansa Navindee <navindeedulansa@gmail.com>
2025-03-09 21:41:30 +0530

    Update ResponsiveAppBar.jsx

--- frontend/src/components/nav&footer/ResponsiveAppBar.jsx
+++ frontend/src/components/nav&footer/ResponsiveAppBar.jsx
@@ -1,1 +1,95 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+
+const pages = ["About Us", "Events", "Contact Us"];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#fff5f0" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* Left side - Logo and Text */}
+          <Box sx={{ flexGrow: 0, display: "flex", alignItems: "center" }}>
+            <img src="assests/logo.png" className="w-15" />
+            <Typography
+              variant="h6"
+              sx={{color: "red", fontWeight: "bold" }} 
+            >PledgeIt
+
+            </Typography>
+          </Box>
+
+          {/* Mobile Menu - Align Left */}
+          <Box sx={{ ml:"auto", display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="black"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          {/* Right side - Menu Items */}
+          <Box sx={{ ml: "auto", display: { xs: "none", md: "flex"  },gap:4 }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "black", display: "block"  }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
 
